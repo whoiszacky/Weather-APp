c42c11d34f197e24cc6737bdcab8b73fcb908432
whoiszacky <moahmmedgcal7@gmail.com>
2023-11-17 10:12:33 +0200

    Updated!!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const currentDate = new Date();
 const dateOptions = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
 const formattedDate = currentDate.toLocaleDateString('en-US', dateOptions);
@@ -17,35 +19,34 @@ function hideElementOnSmallScreens() {
   // Call the function once on load
   hideElementOnSmallScreens();
   
-  // And also add an event listener to run it whenever the screen size changes
+  // add an event listener to run it whenever the screen size changes
   window.addEventListener("resize", hideElementOnSmallScreens);
-
- /* const inputElement = document.querySelector("#inputId");
-  let inputArvo = inputElement.value;
-  console.log(inputArvo);*/
 
 import {API_KEY} from "./info.js";
 
-// console.log(API_KEY)  // use console.log () to make sure the api is imported 
-let currentCity = "lahti";
-
 async function getWeatherStatus() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async function(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
 
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude},${longitude}&aqi=no`);
+      const result = await response.json();
 
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${currentCity}&aqi=no`);
-  const result = await response.json();
+      const placeName = result.location.name;
+      document.getElementById("city").innerHTML = placeName;
 
-  const placeName = result.location.name;
-  document.getElementById("city").innerHTML = placeName;
+      const temperatureCelsius = result.current.temp_c;
+      document.getElementById("temp").innerHTML = `${temperatureCelsius} <sup>&#8451;</sup>`;
 
-  const temperatureCelsius = result.current.temp_c;
-  document.getElementById("temp").innerHTML = `${temperatureCelsius} <sup>&#8451;</sup>`;
+      const weatherIconUrl = "http:" + result.current.condition.icon;
+      document.getElementById("weatherIcon").src = weatherIconUrl;
 
-  const weatherIconUrl = "http:" + result.current.condition.icon;
-  document.getElementById("weatherIcon").src = weatherIconUrl;
-
-  const conditionText = result.current.condition.text;
-  document.getElementById("condition").innerHTML = conditionText;
+      const conditionText = result.current.condition.text;
+      document.getElementById("condition").innerHTML = conditionText;
+    });
+  } 
 }
 
 getWeatherStatus();
+
